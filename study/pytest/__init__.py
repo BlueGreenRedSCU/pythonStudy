7fbf1ca067900c41670baed97d4085872abb57cb
Lan Ritong <ritong.lan@shopee.com>
2025-02-12 11:20:12 +0800

    update

---
+++ study/pytest/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+# -- coding: utf-8 --
+# @Time : 2024/12/10 10:39 AM
+# @Author : ritong.lan
+# @File : __init__.py.py
---
+++ study/pytest/learn_pytest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+# -- coding: utf-8 --
+# @Time : 2024/12/10 10:39 AM
+# @Author : ritong.lan
+# @File : learn_pytest.py
+
